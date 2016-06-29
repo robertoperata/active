1cb14df9ab80f4c38f92438b4b5219a84558cd74
robertoperata <robypriz@gmail.com>
2016-06-29 11:58:06 +0200

    form type per registrazione

---
+++ src/BookManagerBundle/Form/RegistrationType.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: roberto
+ * Date: 29/06/16
+ * Time: 11.50
+ */
+
+namespace BookManagerBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class RegistrationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('cell_number');
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+
+        // Or for Symfony < 2.8
+        // return 'fos_user_registration';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+
+    // For Symfony 2.x
+    public function getCellNumber()
+    {
+        return $this->getBlockPrefix();
+    }
+}
